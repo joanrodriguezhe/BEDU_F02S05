fe9d989b73317d2afe3c7d57ebf8e702466b6c6b
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-01-20 18:42:33 -0700

    Ejemplo 01

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 //Ejemplo 1
 
-const Person = function(name, birthYear, job){
-    this.name=name;
-    this.birthYear=birthYear;
-    this.job=job;
+const Person = function (name, birthYear, job) {
+    this.name = name;
+    this.birthYear = birthYear;
+    this.job = job;
 }
 
-let john=new Person('John', 1990, 'Developer');
-let jane=new Person('Jane', 1975, 'Designer');
-let mark=new Person('Mark', 1985, 'Teacher');
+let john = new Person('John', 1990, 'Developer');
+let jane = new Person('Jane', 1975, 'Designer');
+let mark = new Person('Mark', 1985, 'Teacher');
 
-console.log(john, jane, mark)
+/* console.log(john);
+console.log(jane);
+console.log(mark); */
+
+
+//Reto 1
+var Vec = function (x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Vec.prototype.plus = () => {
+    return this.x + this.y;
+}
+
+var vec1 = new Vec(1, 2);
+var vec2 = new Vec(2, 3);
+
+console.log(vec1.plus(vec2)); // Vec { x: 3, y: 5 }
+/* console.log(vec1.minus(vec2)); // Vec { x: -1, y: -1 }
+console.log(vec1.length()); // 2.23606797749979 */
